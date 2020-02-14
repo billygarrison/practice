78d4034c2108f185581a87e426181818f14c7ded
Billy Garrison <billygarrison.btc@gmail.com>
2020-02-14 15:08:38 -0400

    Video 22: Update colour based on API results

--- python/tkinter/weather.py
+++ python/tkinter/weather.py
@@ -8,7 +8,6 @@ from PIL import ImageTk, Image  # Non-standard library `Pillow`
 root = Tk()
 root.title("Weather App Exercise")
 root.iconbitmap("images/favicon.ico")
-root.configure(background="green")
 
 # API URL
 api_url = (
@@ -23,17 +22,42 @@ api_url = (
 try:
     api_request = requests.get(api_url)
     api_content = json.loads(api_request.content)
+
+    # Parse API content
+    city = api_content[0]["ReportingArea"]
+    quality = str(api_content[0]["AQI"])
+    category = api_content[0]["Category"]["Name"]
+
+    # Background colour based on AQI
+    if category == "Good":
+        background_colour = "#0C0"
+    elif category == "Moderate":
+        background_colour = "#FFFF00"
+    elif category == "Unhealthy for Sensitive Groups":
+        background_colour = "#FF9900"
+    elif category == "Unhealthy":
+        background_colour = "#FF0000"
+    elif category == "Very Unhealthy":
+        background_colour = "#990066"
+    elif category == "Hazardous":
+        background_colour = "#660000"
+
+    # Update program colour
+    root.configure(background=background_colour)
+
+    # Add API content to Label
+    lbl_text = city + " Air Quality: " + quality + " (" + category + ")"
+    lbl = Label(
+        root, 
+        text=lbl_text,
+        font=("Helvetica", 20),
+        background=background_colour
+    )
+    lbl.grid(row=0, column=0, padx=10, pady=10)
 except Exception as e:
     aqi_content = "Error..."
 
-# Parse API content
-city = api_content[0]["ReportingArea"]
-quality = str(api_content[0]["AQI"])
-category = api_content[0]["Category"]["Name"]
 
-# Add API content to Label
-lbl = Label(root, text=city + " Air Quality: " + quality + " (" + category + ")", font=("Helvetica", 20), background="green")
-lbl.grid(row=0, column=0, padx=10, pady=10)
 
 # Main loop
 root.mainloop()
