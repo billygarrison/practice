9d95805e2e29490d4bb43cda70c93c7cb00bfb4c
Billy Garrison <billygarrison.btc@gmail.com>
2020-02-19 09:08:53 -0400

    Add fields to table in setup

--- python/tkinter/crm-tool/setup.py
+++ python/tkinter/crm-tool/setup.py
@@ -6,7 +6,7 @@ c = conn.cursor()
 
 # Create table
 c.execute(
-    "CREATE TABLE customers ("
+    "CREATE TABLE IF NOT EXISTS customers ("
     + "first_name TEXT, "
     + "last_name TEXT, "
     + "zipcode INTEGER, "
@@ -14,3 +14,18 @@ c.execute(
     + "user_id INTEGER PRIMARY KEY AUTOINCREMENT"
     + ")"
 )
+
+# Add more fields
+new_fields = [
+    "email TEXT",
+    "address_1 TEXT",
+    "address_2 TEXT",
+    "city TEXT",
+    "state TEXT",
+    "country TEXT",
+    "phone TEXT",
+    "payment_method TEXT",
+    "discount_code TEXT",
+]
+for field in new_fields:
+    c.execute("ALTER TABLE customers ADD COLUMN " + field)
